6cd4b3b4fe11ccfae1a256d7f1db4a2ef78d6993
Angus <angus.goody@gmail.com>
2023-05-21 09:04:08 +0100

    Added updated homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,11 @@ class HomeController extends Controller
     public function index(Request $request): View
     {
         return view('welcome', [
-            'reports' => ["Report1", "Report2", "Report3"],
+            'reports' => [
+                ["name" => "Bob", "location" => "London", "time" => "12:45"],
+                ["name" => "Charlie", "location" => "Essex", "time" => "15:05"],
+                ["name" => "Dave", "location" => "Sheffield", "time" => "19:12"]
+            ]
         ]);
     }
 }
