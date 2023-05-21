762dd95ddfe2c8a9ed50187dc10be70eb249444e
Angus <angus.goody@gmail.com>
2023-05-21 09:31:18 +0100

    Added endpoint

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    //
+    /**
+     * Store a new report in the database
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'lat' => 'required',
+            'long' => 'required',
+        ]);
+
+
+        // Create a new report associated with the panic button press
+        Report::create([
+            'name' => $validatedData['name'],
+            'long' =>  $validatedData['long'],
+            'lat' =>  $validatedData['lat']
+        ]);
+
+        // Return a 201 response
+        return response(null, 201);
+    }
 }
