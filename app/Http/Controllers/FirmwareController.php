13ed212c00b0d035618555ef8c10ff15a242614f
Angus <angus.goody@gmail.com>
2023-05-22 08:44:56 +0100

    Added firmware download endpoint

--- app/Http/Controllers/FirmwareController.php
+++ app/Http/Controllers/FirmwareController.php
@@ -16,10 +16,41 @@ class FirmwareController extends Controller
     }
 
     /**
-     * Download firmware
+     * Download firmware endpoint
      */
     public function download($version="latest")
     {
-        return response()->json(['version to download' => $version]);
+
+        // Get the latest firmware
+        if ($version === "latest") {
+            // Logic when version is not provided
+            // For example, you can retrieve the latest firmware version from the environment variable
+
+            $latestVersion = config('app.firmware_version');
+            $firmwareFiles = glob(public_path('firmware/*.bin'));
+
+            foreach ($firmwareFiles as $file) {
+                $filename = basename($file);
+                $fileVersion = (int) pathinfo($filename, PATHINFO_FILENAME);
+
+                if ($fileVersion > $latestVersion) {
+                    $latestVersion = $fileVersion;
+                }
+            }
+
+            $version = $latestVersion;
+        }
+
+        // Attempt to find the binary in the public folder
+        $filename = $version . '.bin';
+
+        $filePath = public_path('firmware/' . $filename);
+
+        if (file_exists($filePath)) {
+            return response()->file($filePath);
+        } else {
+            // Handle case when the firmware file does not exist
+            abort(404, 'Firmware not found');
+        }
     }
 }
